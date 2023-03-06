6461ce2e8024259f6bb595ada8be2a3e45bc14b1
736327363@qq.com <736327363@qq.com>
2023-03-06 13:40:36 +0800

    final version

--- src/router/index.js
+++ src/router/index.js
@@ -8,37 +8,37 @@ import UserListView from "../views/UserListView";
 
 const routes = [
   {
-    path: '/',
+    path: '/myspace/',
     name: 'home',
     component: HomeView
   },
   {
-    path: '/login/',
+    path: '/myspace/login/',
     name: 'login',
     component: LoginView
   },
   {
-    path: '/404/',
+    path: '/myspace/404/',
     name: '404',
     component: NotFoundView
   },
   {
-    path: '/register/',
+    path: '/myspace/register/',
     name: 'register',
     component: RegisterView
   },
   {
-    path: '/userprofile/:userId/',
+    path: '/myspace/userprofile/:userId/',
     name: 'userprofile',
     component: UserProfileView
   },
   {
-    path: '/userlist/',
+    path: '/myspace/userlist/',
     name: 'userlist',
     component: UserListView
   },
   {
-    path: '/:catchAll(.*)',
+    path: '/myspace/:catchAll(.*)',
     redirect: '/404/'
   }
 ]
